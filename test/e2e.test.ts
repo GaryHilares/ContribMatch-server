acb0c903cb431fe1f834ed25251819113b577f9b
Gary Hilares <dev.garyhilares@gmail.com>
2025-08-10 00:07:20 -0500

    Added more e2e tests

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -4,8 +4,17 @@ import request from 'supertest';
 
 describe('End-to-end tests', () => {
   describe('DELETE /users/:userId', () => {
+    test('should reject invalid negative numeric format with 401 status code', () => {
+      return request(app).delete('/users/-37').expect(401);
+    });
+    test('should reject invalid decimal numeric format with 401 status code', () => {
+      return request(app).delete('/users/3.7').expect(401);
+    });
     test('should reject invalid format with 401 status code', () => {
       return request(app).delete('/users/th1s-1s-not!-v@l1d').expect(401);
+    });
+    test('should reject valid inexisting ID with 404 status code', () => {
+      return request(app).delete('/users/10000000000').expect(404);
     });
   });
 });
